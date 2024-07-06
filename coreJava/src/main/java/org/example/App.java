2df93e0976c15db01fd38942458baf58885733cf
S4satheesh <satheeshn001@gmail.com>
2024-07-06 20:45:08 +0530

    Commiting for the first time.

---
+++ coreJava/src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("First print out after pull from git.");
+    }
+}
